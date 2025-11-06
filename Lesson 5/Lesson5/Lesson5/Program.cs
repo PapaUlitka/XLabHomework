f72670b688e6f1157ca328a1c1902671876c05c3
Oleg Polishchuk <o.polishchuk@xlab-game.com>
2025-11-06 16:29:57 +0300

    Lesson5Commit

---
+++ Lesson 5/Lesson5/Lesson5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Lesson5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IDamagable<Zombie> zombie = new Stack<Monster>();
+            zombie.TakeDamage(new Zombie());
+        }
+    }
+    public abstract class Monster { }
+    public class Zombie : Monster { }
+    public interface IDamagable<in T> where T : Monster
+    {
+        void TakeDamage(T damage);
+    }
+    class Stack<T> : IDamagable<T> where T : Monster
+    {
+        public void TakeDamage(T damage)
+        {
+
+        }
+    }
+}
